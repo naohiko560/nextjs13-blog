5410b06b1bf5026a975f6809f31e3b3ff4fd7bf9
naohiko560 <66761935+naohiko560@users.noreply.github.com>
2023-05-15 16:32:38 +0900

    .env.local修正

--- src/lib/gtag.tsx
+++ src/lib/gtag.tsx
@@ -1,10 +1,10 @@
 // Google Analytics 測定ID
-export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID || '';
+export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID || "";
 
-export const existsGaId = GA_MEASUREMENT_ID !== '';
+export const existsGaId = GA_MEASUREMENT_ID !== "";
 
 export const pageview = (path: string) => {
-  window.gtag('config', GA_MEASUREMENT_ID, {
+  window.gtag("config", GA_MEASUREMENT_ID, {
     page_path: path,
   });
 };
