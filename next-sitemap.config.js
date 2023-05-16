60d6eba12720c884acfce3d56d6638a5eb07b516
naohiko560 <66761935+naohiko560@users.noreply.github.com>
2023-05-16 09:37:23 +0900

    サイトマップの設定完了

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://nextjs13-blog-ashen.vercel.app/',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
