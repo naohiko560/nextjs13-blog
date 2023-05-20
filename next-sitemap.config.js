9412d49abee63efda3a1bba101ea2816e8c5e703
naohiko560 <66761935+naohiko560@users.noreply.github.com>
2023-05-20 16:45:39 +0900

    サイトマップをpublicに変更

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,4 @@ module.exports = {
   siteUrl: process.env.SITE_URL || 'https://www.nhackblog.com/',
   changefreq: 'weekly',
   generateRobotsTxt: true,
-  sitemapSize: 7000,
-  outDir: './out',
 };
